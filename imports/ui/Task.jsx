5955cbecae99fe8f6cbfb31084d7a1e47524e345
triyanarief <triyanarief@icloud.com>
2017-03-28 21:43:51 +0700

    test render

---
+++ imports/ui/Task.jsx
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+// Task component represent single item
+export default class Task extends Component {
+  render() {
+    return (
+      <li>{this.props.task.text}</li>
+    );
+  }
+}
+
+Task.propTypes = {
+  // This component gets the task to display through a react prop
+  // we can use protoTypes to indicates it is required
+  task: PropTypes.object.isRequired,
+};
